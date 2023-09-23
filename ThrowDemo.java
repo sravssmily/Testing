5eed395387dd5638aa4f00f4817fc51cc630eaba
sravssmily <145099515+sravssmily@users.noreply.github.com>
2023-09-23 11:53:13 +0530

    Create ThrowDemo.java

---
+++ ThrowDemo.java
@@ -0,0 +1,27 @@
+package exceptionsPrograms;
+
+public class ThrowDemo {
+	
+	
+	public void div(int a , int b)
+	{
+		if (b==0)
+		{
+			throw new ArithmeticException();
+		}
+		else
+		{
+			int c = a/b;
+			System.out.println(c);
+			
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		ThrowDemo t = new ThrowDemo();
+		t.div(20, 0);  // it will throw an exception as division by 0 .
+		
+	}
+
+}
