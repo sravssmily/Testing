280c086ff78cb05621563c052ebda78229e16eda
sravssmily <145099515+sravssmily@users.noreply.github.com>
2023-09-23 12:02:08 +0530

    Create Arraydemo.java

---
+++ collectonsDemo/Arraydemo.java
@@ -0,0 +1,26 @@
+package collectionsDemo;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+	
+		
+		String s1 = "vaule1";
+		
+		int i1 = 10;
+		
+		
+		
+		int [] arr1 = {10,20,30,40,50};
+		
+		int len = arr1.length;
+		
+		for(int i =0; i<len;i++)
+		{
+		System.out.println(arr1[i]);
+		
+		}
+		
+	}
+
+}
