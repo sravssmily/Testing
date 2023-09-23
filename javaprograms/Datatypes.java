3558dc0e3cec2c8c0c206c5e9501abb6b0ed8c29
sravssmily <145099515+sravssmily@users.noreply.github.com>
2023-09-23 12:16:47 +0530

    Create Datatypes.java

---
+++ javaprograms/Datatypes.java
@@ -0,0 +1,26 @@
+package javaPrograms;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		
+		int var1 = 10;   // integer
+		double var2 = 2.3;   // decimal value
+		char var3 = 'A';   // character
+		String var4 = "Selenium";  //string
+		boolean var5 = true;  // boolean
+
+		// write the statement to print the variable value
+		
+		System.out.println("The value of variable var1: " + var1);
+		System.out.println("The value of variable var2: " + var2);
+		System.out.println("The value of variable var3: " + var3);
+		System.out.println("The value of variable var4: " + var4);
+		System.out.println("The value of variable var5: " + var5);
+		
+		
+		
+	}
+
+}
+
